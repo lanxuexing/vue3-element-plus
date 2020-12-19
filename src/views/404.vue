be1a9421a90018c293dba3c538412ffeeaf9f7bf
lanxuexing <lanxuexing313wsr@163.com>
2020-12-19 20:41:15 +0800

    feat: 基本结构

---
+++ src/views/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="not-found">
+    <img src="@/assets/404.gif" alt="404" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "404",
+  components: {},
+});
+</script>
+
+<style scoped>
+.not-found {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.not-found img {
+  width: 100%;
+  height: 100%;
+}
+</style>
